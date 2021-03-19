d5d50bca2b5e89688be00d48b1efdb35613ea960
li <lideng928@gmail.com>
2021-03-18 20:50:32 -0400

    Complete registration controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,58 @@
 //request the user model
-var User = require('../models/user');
+var User = require("../models/user");
 
 //Display log in form on GET
-exports.log_in_get = function(req, res){
-    console.log('Get login');
-    res.render('log_in');
-}
+exports.log_in_get = function (req, res) {
+  console.log("Get login");
+  res.render("log_in");
+};
 
 //Log in post
-exports.log_in_post = function(req, res){
-    var user = req.body.username;
-    var pwd = req.body.password;
-    User.findOne({username: user, password: pwd}, function(err, item){
-        if(err)console.log(err);
-        if(item){
-            req.session.username = item.username;
-            req.session.userLoggedIn = true;
-            res.render('success', {action: "You have logged in"});
-        }
-        else{
-            res.render('log_in', {error: 'Please try again.'});
-        }
-    });
+exports.log_in_post = function (req, res) {
+  var user = req.body.username;
+  var pwd = req.body.password;
+  User.findOne({ username: user, password: pwd }, function (err, item) {
+    if (err) console.log(err);
+    if (item) {
+      req.session.username = item.username;
+      req.session.userLoggedIn = true;
+      res.render("success", { action: "You have logged in" });
+    } else {
+      res.render("log_in", { error: "Please try again." });
+    }
+  });
 };
 
 //Log out
-exports.log_out_get = function(req, res){
-    req.session.destroy();
-    //redirect back to index page
-    res.render('success', {action: 'Logged out'});
-}
+exports.log_out_get = function (req, res) {
+  req.session.destroy();
+  //redirect back to index page
+  res.render("success", { action: "Logged out" });
+};
+
+//Register a new user
+exports.register_get = function (req, res) {
+  console.log("Get registered");
+  //Get the register_form from views
+  res.render("register_form");
+};
+
+//Register a new user - POST
+exports.register_post = function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  pageData = {
+    username: username,
+    password: password,
+  };
+
+  //Pass data and create a new user model instance
+  var newUser = new User(pageData);
+
+  //Save the new product to MongoDB
+  newUser.save().then(() => console.log("New user created!"));
+
+  //Renders a success message
+  res.render("success", { action: "Created a new user" });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var user_controller = require('../controllers/userController');
+var user_controller = require("../controllers/userController");
 
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
 
-router.get('/', user_controller.log_in_get);
+router.get("/", user_controller.log_in_get);
 
-router.post('/', user_controller.log_in_post);
+router.post("/", user_controller.log_in_post);
 
-router.get('/logout', user_controller.log_out_get);
+router.get("/logout", user_controller.log_out_get);
+
+router.get("/register", user_controller.register_get);
+
+router.post("/register", user_controller.register_post);
 
 module.exports = router;
