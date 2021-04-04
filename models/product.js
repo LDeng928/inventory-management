70f2c48aeecb83c0fa4019df84ea91f68352b3fb
li <lideng928@gmail.com>
2021-04-04 17:44:47 -0400

    edit product controller

--- models/product.js
+++ models/product.js
@@ -1,17 +1,17 @@
 // Require mongoose
-var mongoose = require('mongoose');
-const { Int } = require('mssql');
+var mongoose = require("mongoose");
+//const { Int } = require('mssql');
 
 // Set up Product model
-const Product = mongoose.model('Product', {
-    productId: Number,
-    productName: String,
-    productPrice: Number,
-    productCost: Number,
-    category: String,
-    storeName: String,
-    warehouseQty: Number,
-    storeQty: Number
+const Product = mongoose.model("Product", {
+  productId: Number,
+  productName: String,
+  productPrice: Number,
+  productCost: Number,
+  category: String,
+  storeName: String,
+  warehouseQty: Number,
+  storeQty: Number,
 });
 
 //Export the model
