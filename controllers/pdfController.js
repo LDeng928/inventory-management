8f02c6c87a90fdcbbeca5391ad9d00b90ee4b4cb
li <lideng928@gmail.com>
2021-04-07 14:34:48 -0400

    complete pdfController

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,35 +1,40 @@
 let ejs = require("ejs");
+const { json } = require("express");
 let pdf = require("html-pdf");
 let path = require("path");
 //const { options } = require("../app");
-let orders = require("../models/order");
+let Order = require("../models/order");
 
 exports.generate_report = function (req, res) {
-  ejs.renderFile(
-    path.join(__dirname, "../views/", "report-template.ejs"),
-    { orders: orders },
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-        let options = {
-          height: "11.25in",
-          width: "8.5in",
-          header: {
-            height: "20mm",
-          },
-          footer: {
-            height: "20mm",
-          },
-        };
-        pdf.create(data, options).toFile("report.pdf", function (err, data) {
-          if (err) {
-            res.send(err);
-          } else {
-            res.send("File created successfully.");
-          }
-        });
+  Order.find({}).exec(function (err, orders) {
+    ejs.renderFile(
+      path.join(__dirname, "../views/", "report-template.ejs"),
+      { orders: orders },
+      (err, data) => {
+        if (err) {
+          res.send(err);
+        } else {
+          let options = {
+            height: "11.25in",
+            width: "8.5in",
+            header: {
+              height: "20mm",
+            },
+            footer: {
+              height: "20mm",
+            },
+          };
+          pdf.create(data, options).toFile("report.pdf", function (err, data) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.render("success", {
+                action: "An order list report was created ",
+              });
+            }
+          });
+        }
       }
-    }
-  );
+    );
+  });
 };
