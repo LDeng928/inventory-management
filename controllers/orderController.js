6c253c04fe39d246ee08f1be94b1672f61763f41
li <lideng928@gmail.com>
2021-04-12 13:41:41 -0400

    add order update get and post

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -61,16 +61,74 @@ exports.order_create_post = function (req, res) {
 
 // order delete get
 exports.order_delete_get = function (req, res) {
-  res.send("Not implemented");
+  //res.send("Not implemented");
+  if (req.session.userLoggedIn) {
+    var id = req.params.id;
+
+    Order.findByIdAndDelete({ _id: id }).exec(function (err, order) {
+      if (order) {
+        res.render("success", { action: "Order removed." });
+      } else {
+        res.render("error", {
+          message: "Order not found. Please try again.",
+        });
+      }
+    });
+  }
+  //Else redirect back to log in page
+  else {
+    res.redirect("/users");
+  }
 };
 
 // order update get
 exports.order_update_get = function (req, res) {
-  res.send("Not implemented");
+  //res.send("Not implemented");
+  var id = req.params.id;
+
+  if (req.session.userLoggedIn) {
+    Order.findOne({ _id: id }).exec(function (err, order) {
+      if (err) console.log(err);
+
+      var pageData = {
+        productId: order.productId,
+        productName: order.productName,
+        category: order.category,
+        orderQty: order.orderQty,
+        productPrice: order.productPrice,
+        orderTotal: order.orderTotal,
+      };
+      res.render("order_form", pageData);
+    });
+  } else {
+    res.redirect("/users");
+  }
 };
 
 exports.order_update_post = function (req, res) {
-  res.send("Not implemented");
+  //res.send("Not implemented");
+  var productId = req.body.productId;
+  var productName = req.body.productName;
+  var category = req.body.category;
+  var orderQty = req.body.orderQty;
+  var productPrice = req.body.productPrice;
+  var orderTotal = req.body.orderTotal;
+
+  var pageData = {
+    productId: productId,
+    productName: productName,
+    category: category,
+    orderQty: orderQty,
+    productPrice: productPrice,
+    orderTotal: orderTotal,
+  };
+
+  var query = { _id: req.params.id };
+
+  Order.updateOne(query, pageData, (err, doc) => {
+    if (err) console.log(err);
+    res.render("success", { action: "Updated order" });
+  });
 };
 
 // order detail
