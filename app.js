8f01d5001fa87fd9b0676891db0cef3bb5a9b02a
Li Deng <lideng928@gmail.com>
2024-02-14 23:29:28 -0500

    Updated MongoDB connection and stored the connection key in an env file

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
 //var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var fileUpload = require("express-fileupload");
-var session = require("express-session");
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+var session = require('express-session');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var storeRouter = require("./routes/store");
-var productRouter = require("./routes/product");
-var orderRouter = require("./routes/order");
-var pdfRouter = require("./routes/pdf");
-var reportRouter = require("./routes/report");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var storeRouter = require('./routes/store');
+var productRouter = require('./routes/product');
+var orderRouter = require('./routes/order');
+var pdfRouter = require('./routes/pdf');
+var reportRouter = require('./routes/report');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(bodyParser());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 app.use(
   session({
-    secret: "superrandomsecret",
+    secret: 'superrandomsecret',
     resave: true,
     saveUninitialized: true,
     cookie: { maxAge: 24 * 60 * 60 * 1000 },
   })
 );
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/store", storeRouter);
-app.use("/product", productRouter);
-app.use("/order", orderRouter);
-app.use("/pdf", pdfRouter);
-app.use("/report", reportRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/store', storeRouter);
+app.use('/product', productRouter);
+app.use('/order', orderRouter);
+app.use('/pdf', pdfRouter);
+app.use('/report', reportRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -54,11 +54,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
--- models/db.js
+++ models/db.js
@@ -1,16 +1,19 @@
-//set up MongoDB connection 
-//import the mongoose module 
+//set up MongoDB connection
+//import the mongoose module
+require('dotenv').config();
 const mongoose = require('mongoose');
 //set up MongoDB Altas connection
-var mongoDB = 'mongodb+srv://coolUser:coolPassword@capstoneims.lyj4y.mongodb.net/CapstoneIMS?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const mongoDB = process.env.MONGO_DB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
 var db = mongoose.connection;
 //Show message
-db.once('open', (err) => {console.log('db connected')});
-//Console log errors 
+db.once('open', (err) => {
+  console.log('db connected');
+});
+//Console log errors
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
-//Export module 
+//Export module
 module.exports = db;
-
